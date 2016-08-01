945992e8433e6c07a4c1b3458c3eb0c8094b7dfe
Quentin Stoeckel <chteuchteu@gmail.com>
2016-08-01 10:16:36 +0200

    Use MaterialDesignIcons-Upstream-Parser

--- generate.py
+++ generate.py
@@ -4,24 +4,21 @@
 import sys
 import urllib.request
 import re
+from upstream_parser import mdi_upstream
 
 icon_prefix = 'mdi-'
-input_file_uri = 'https://raw.githubusercontent.com/Templarian/MaterialDesign-Webfont/master/scss/_variables.scss'
 output_file = 'dist/_materialdesignicons-vars.scss'
 output_header = """/**
  * MaterialDesignIcons-SCSS-Variables
  * https://github.com/chteuchteu/MaterialDesignIcons-SCSS-Variables
+ *
+ * MaterialDesignIcons v#VERSION#
  */\n\n"""
 
 # Download & parse input file
-response = urllib.request.urlopen(input_file_uri)
-data = response.read()
-input = data.decode('utf-8')
+meta = mdi_upstream.fetch_meta()
 
-regex = re.compile('    "(.*)": (F[A-F0-9]*),?')
-matches = regex.findall(input)
-
-if len(matches) == 0:
+if len(meta['icons']) == 0:
     print('Could not find variables.')
     sys.exit(1)
 
@@ -29,12 +26,12 @@ if len(matches) == 0:
 with open(output_file, 'w') as output:
     output.truncate()
 
-    output.writelines(output_header)
+    output.writelines(output_header.replace('#VERSION#', meta['version']))
 
-    for match in matches:
-        icon_name = match[0]
-        icon_hex = match[1]
+    for icon in meta['icons']:
+        icon_name = icon['name']
+        icon_hex = icon['codepoint']
 
         output.write('${}: "\{}";\n'.format(icon_prefix + icon_name, icon_hex))
 
-    print("Generated {} with {} variables".format(output_file, len(matches)))
+    print("Generated {} with {} variables".format(output_file, len(meta['icons'])))
